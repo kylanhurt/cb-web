b4ec1798b19e628522e323303b947eecaf171a6f
kylanhurt <kylan.hurt@gmail.com>
2018-09-24 22:57:43 -0700

    Removal of /dist files foer the time being

--- dist/locales/default.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const en_US_js_1 = require("./en_US.js");
-const strings = Object.assign({}, en_US_js_1.default);
-exports.default = strings;
-//# sourceMappingURL=default.js.map
